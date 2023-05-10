b1b80020d938b7fe06ddf2d86eb936308bd2d724
fateme hashemi <fateme.7575h@gmail.com>
2023-05-10 12:43:46 +0330

    clientinf

---
+++ ClientGetInfomt4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dllGetInfomt4;
+using TradingAPI.MT4Server;
+namespace ClientGetInfomt4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            new Program().Run();
+        }
+        void Run()
+        {
+            try
+            {
+                //MainServer srv = QuoteClient.LoadSrv(
+                //@"C:\Program Files (x86)\Opo group MetaTrader 43\config\OpogroupLLC-DEMO.srv");
+                QuoteClient qc = new QuoteClient(6603764, "I3qcec01", "185.65.175.144", 1950);
+                Console.WriteLine("Connecting...");
+                qc.Connect();
+                Console.WriteLine("Connected to server");
+                qc.OnQuote += new QuoteEventHandler(qc_OnQuote);
+                qc.Subscribe("EURUSD!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+        void qc_OnQuote(object sender, QuoteEventArgs args)
+        {
+            Console.WriteLine(args.Symbol + " " + args.Bid + " " + args.Ask);
+            Console.ReadKey();
+        }
+
+    }
+}
